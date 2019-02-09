14644fc8600818841cae6e0e8425d8f6211c4d1c
grazka <grazka@192.168.1.2>
2019-02-09 20:18:01 +0100

    zmiana dodanie trzeciej linii

--- src/pakiet/GITTestApplication.java
+++ src/pakiet/GITTestApplication.java
@@ -6,6 +6,7 @@ public class GITTestApplication {
 		// TODO Auto-generated method stub
 		System.out.println("Hello GIT");
 		System.out.println("Hello GIT New");
+		System.out.println("Hello Git, from branch bug#1234 !!");
 	}
 
 }
